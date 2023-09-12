1a958ab16c50b23b2291990da10a387649db3dbb
Apunich Bhagwan Hai <aryan.bhavsar2909@gmail.com>
2023-09-12 17:40:04 +0530

    first commit bubble sort

--- bubblesort.java
+++ bubblesort.java
@@ -1,5 +1,5 @@
 public class bubblesort {
     public static void main(String[] args) {
-        System.out.println("this is demo git hub repository checking project first commit 1");
+        System.out.println("this is demo git hub repository checking project first commit");
     }
 }
